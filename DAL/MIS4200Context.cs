2647eeef623b00b450a4ee8423e1bb98c5245188
rp799815 rp799815 <rp799815@ohio.edu>
2019-10-02 21:02:09 -0400

    final

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -11,12 +11,14 @@ namespace rp799815_MIS4200.DAL
     {
         public MIS4200Context() : base("name=DefaultConnection")
         {
+            Database.SetInitializer (new MigrateDatabaseToLatestVersion<MIS4200Context,rp799815_MIS4200.Migrations.MISContext.Configuration>("DefaultConnection"));
             // this method is a 'constructor' and is called when a new context is created
             // the base attribute says which connection string to use
         }
         public DbSet<Doctor> Doctor { get; set; }
         public DbSet<Appointment> Appointment { get; set; }
         public DbSet<Patient> Patient { get; set; }
+
     }
 }
         
